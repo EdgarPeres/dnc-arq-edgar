cd16f6c79e4639eb804c3df4c5a6966c04ed04db
EdgarPeres <edgarperesedgar@gmail.com>
2025-10-02 19:43:44 -0300

    create button components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/header/Header"
 import Footer from "../components/Footer/Footer"
+import Button from "../components/Button/Button"
 
 function Home() {
     return (
@@ -7,6 +8,7 @@ function Home() {
             <Header />
             <h1>Home</h1>
             <p>subtitle</p>
+            <Button buttonStyle="primary" arrow >olá</Button>
             <Footer/>
         </>
     )
