0954c4cdc514ad28ce17f8c6d41996b88084583f
EdgarPeres <edgarperesedgar@gmail.com>
2025-10-06 12:52:43 -0300

    Create component banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
 import Header from "../components/header/Header"
 import Footer from "../components/Footer/Footer"
 import Button from "../components/Button/Button"
+import Banner from "../components/Banner/Banner"
 
 function Home() {
     return (
         <>
             <Header />
-            <h1>Home</h1>
-            <p>subtitle</p>
+            <Banner title="Home" image="About.jpg" />
             <Button buttonStyle="primary" arrow >olá</Button>
-            <Footer/>
+            <Footer />
         </>
     )
 }
