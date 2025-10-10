dac65bd8db4b4aa4b231ce17f319e08f919fce9b
EdgarPeres <edgarperesedgar@gmail.com>
2025-10-10 06:50:42 -0300

    feat(menu): add mobile navigation menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,40 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 //ASSETS
 import Logo from '../../assets/dnc-logo.svg'
 
+//COMPONENTS
+import Button from '../Button/Button'
+
 //STYLE HEADER
 import './Header.css'
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggleMenu=()=>{
+        setIsOpen(!isOpen)
+    }
     return (
         <header>
             <div className="container">
                 <div className="al-center d-flex jc-space-betwen">
                     <Link to="/"><img src={Logo} /></Link>
-                     <nav>
-                    <ul className='d-flex'>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                        <li><Link to="/projects">Project</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
+                    <div className="mobile-menu">
+                        <Button buttonStyle="secondary" onClick={toggleMenu}>Menu</Button>
+                    </div>
+                    <nav className={`${isOpen ? 'open' : ''}`}>
+                        <Button buttonStyle="unstyled" className="close-btn" onClick={toggleMenu}>X</Button>
+                        <ul className='d-flex'>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/about">About</Link></li>
+                            <li><Link to="/projects">Project</Link></li>
+                            <li><Link to="/contact">Contact</Link></li>
 
-                    </ul>
-                </nav>
+                        </ul>
+                    </nav>
                 </div>
-               
+
             </div>
         </header>
     )
